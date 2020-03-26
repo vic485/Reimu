190ead21bcace8b292c26a146774ed7c0c700e83
Victor Fugazzotto <victorfuga@gmail.com>
2020-03-26 16:35:39 -0400

    Let's play with .NET 5

---
+++ ReimuBot/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Reimu
+{
+    internal static class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            // TODO: Here would be a command line program to manage the bot
+            // Mainly so it can be shutdown safely (close/dispose) all services.
+            // IDisposables may not be disposed on exit
+            await Task.Delay(-1);
+        }
+    }
+}
